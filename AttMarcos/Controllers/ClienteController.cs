9c55b1cc79c2e6d6c7a5694434ca9c1323bac1c3
= <ryannogueiranogueira0@gmail.com>
2024-08-01 16:38:09 -0300

    versão atualizada

---
+++ AttMarcos/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AttMarcos.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AttMarcos/Controllers/NossoAppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AttMarcos.Controllers
+{
+    public class NossoAppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
